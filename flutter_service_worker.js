64e8618cab0eca03889a5d1b3fd81e336989942b
Андрей Катюшин <fess200@yandex.ru>
2022-04-29 10:11:37 +0300

    asdadsasd

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "77847996a2b1d4671fcd3c43e767158e",
-"index.html": "5b430fe1a9032da53c701d68a63d232a",
-"/": "5b430fe1a9032da53c701d68a63d232a",
-"main.dart.js": "2830c7cad70c4e4f65415343d89557fc",
+"index.html": "82bb9bdca7ddedcc64493bbff9e946d2",
+"/": "82bb9bdca7ddedcc64493bbff9e946d2",
+"main.dart.js": "5a5dbb2410342bb357eca392c5b4eada",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
